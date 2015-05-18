93ae09feb9651b150758b951a9d2e220775919ab
root <root@localhost.localdomain>
2015-05-18 05:08:40 -0400

    make wx fly

--- wx.py
+++ wx.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+old_msg = "good"
+
+@app.route("/", methods=['GET', 'POST'])
+def verify():
+    global old_msg
+    print request.method + "======================>>"
+    if request.method == 'GET':
+        old_msg = request.data
+        return request.args.get('echostr', 'no')
+    elif request.method == 'POST':
+        old_msg = request.data
+        print request.data
+        return "OK"
+
+@app.route("/log")
+def log():
+    return old_msg
 
 if __name__ == "__main__":
     app.run()
